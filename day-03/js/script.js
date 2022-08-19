63b9d76ba10cd78f496ca31b97174bfcc540a11f
Jonas Monteiro <mfernandes.jonas@gmail.com>
2022-08-19 02:21:22 -0300

    day03 - customized button v2

--- day-03/js/script.js
+++ day-03/js/script.js
@@ -2,19 +2,26 @@ document.getElementById('button').onmouseover = function(){mouseOver()}
 document.getElementById('button').onmouseout = function(){mouseOut()}
 const body = document.querySelector('body');
 const shadow = document.getElementById('btn');
+const cd = document.getElementById('cd');
+const cont = document.querySelector('.cont');
 const title = 'Created by Jonas Fernandes. Thank you! :)'
 function mouseOver(){
     body.style.background = 'white'
     shadow.style.background = 'white'
     shadow.style.transition = '1s'
     body.style.transition = '1s'
+    cont.style.background = 'white'
+    cont.style.transition = '1s'
 
 }
 function mouseOut(){
-    body.style.background = '#1a1a1a'
+    body.style.background = '#2b2b2b'
     body.style.transition = '1s'
     shadow.style.transition = '1s'
-    shadow.style.background = '#1a1a1a'
+    shadow.style.background = '#2b2b2b'
+    cont.style.background = 'white'
+    cont.style.transition = '1s'
+
 }
 
 function clickFunction(){
